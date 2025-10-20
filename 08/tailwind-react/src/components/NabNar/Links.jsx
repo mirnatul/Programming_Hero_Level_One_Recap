006dfb9591abe3750601969f8b298aa0f5730a0b
mirnatul <www.mirnatul@gmail.com>
2025-10-20 14:16:05 +0600

    responsive navbar and lucide icon explored

--- 08/tailwind-react/src/components/NabNar/Links.jsx
+++ 08/tailwind-react/src/components/NabNar/Links.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Links = ({ route }) => {
     return (
         <div>
-            <li><a href={route.url}>{route.name}</a></li>
+            <li className='hover:bg-yellow-300 p-2'><a href={route.url}>{route.name}</a></li>
         </div>
     );
 };
--- 08/tailwind-react/src/components/NabNar/NavBar.jsx
+++ 08/tailwind-react/src/components/NabNar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Links from './Links';
+import { Menu, X } from 'lucide-react';
 
 
 const navLinks = [
@@ -12,14 +13,33 @@ const navLinks = [
 
 
 const NavBar = () => {
-    return (
-        <nav>
 
-            <ul className='flex justify-around'>
+    const [open, setOpen] = useState(false);
+
+    const links = navLinks.map(route => <Links key={route.url} route={route}></Links>)
+
+    return (
+        <nav className='flex justify-between mx-6 mt-4 text-xl'>
+            <span className='flex' onClick={() => setOpen(!open)}>
                 {
-                    navLinks.map(route => <Links key={route.url} route={route}></Links>)
+                    open ? <X className='md:hidden' /> : <Menu className='md:hidden' />
+                }
+
+                <ul className={`md:hidden absolute p-6 duration-1000 bg-white
+                    ${open ? 'top-10' : '-top-44'}`}>
+                    {links}
+                </ul>
+
+
+                <h1>My Navbar</h1>
+            </span>
+
+            <ul className='md:flex gap-6 hidden'>
+                {
+                    links
                 }
             </ul>
+            <button className='btn btn-soft'>Sign In</button>
             {/* <ul className='flex justify-around'>
                 {
                     navLinks.map(route => <li><a href={route.url}>{route.name}</a></li>)
