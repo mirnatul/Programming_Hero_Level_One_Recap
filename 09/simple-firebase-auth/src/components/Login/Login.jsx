bc04acc84a3a2428e54f41e69b4139b0624b38fc
mirnatul <www.mirnatul@gmail.com>
2025-11-03 02:24:46 +0600

    google authentication completed

--- 09/simple-firebase-auth/src/components/Login/Login.jsx
+++ 09/simple-firebase-auth/src/components/Login/Login.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
+    const [user, setUser] = useState(null);
+
+    const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
-        alert("Google sign in");
+        signInWithPopup(auth, provider)
+            .then(result => {
+                console.log(result.user);
+                setUser(result.user)
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                console.log("signout completed");
+                setUser(null);
+            })
+            .catch((error) => { console.log(error); })
+    }
+
+
     return (
         <div>
             <h2>Login</h2>
@@ -12,6 +35,18 @@ const Login = () => {
             <button
                 onClick={handleGoogleSignIn}
                 className='btn btn-primary'>Sign in with google</button>
+            <button
+                onClick={handleSignOut}
+                className='btn btn-secondary mx-2'>Sign Out</button>
+
+            {
+                user && <div>
+                    <h3>{user?.displayName}</h3>
+                    <h3>{user?.email}</h3>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
+
         </div>
     );
 };
